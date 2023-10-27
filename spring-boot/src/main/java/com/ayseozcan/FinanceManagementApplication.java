aa3d75b5defd25f39fa691c36f800a7c2409a211
ayse-ozcan <ayseozcan.1@icloud.com>
2023-10-27 19:38:17 +0300

    first commit

---
+++ spring-boot/src/main/java/com/ayseozcan/FinanceManagementApplication.java
@@ -0,0 +1,14 @@
+package com.ayseozcan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class })
+public class FinanceManagementApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FinanceManagementApplication.class, args);
+    }
+
+}
